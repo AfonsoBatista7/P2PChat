2195b52ca5f87e04165d68788846db18c47ec1f0
Afonso Batista <afonsobatista13@gmail.com>
2025-06-24 15:36:39 +0100

    [Update] - added Relay peer to project

--- go-code/peer_manager.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// PeerManager struct to manage peer state
-type PeerManager struct {
-	done        chan bool // Channel to signal termination
-	disconnect  chan bool // Channel to signal termination
-	unsubscribe chan bool // Channel to signal termination
-}
