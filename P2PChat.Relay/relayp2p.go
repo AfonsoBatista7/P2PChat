e55e2bb6faa7f7dd8a045d1a63ba453eea3a1d9b
Afonso Batista <afonsobatista13@gmail.com>
2025-07-09 20:30:28 +0100

    [Update] - relay notifying when connected and disconnected

--- P2PChat.Relay/relayp2p.go
+++ P2PChat.Relay/relayp2p.go
@@ -11,6 +11,7 @@ import (
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	"github.com/libp2p/go-libp2p/p2p/discovery/util"
 	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
@@ -54,6 +55,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Set up network notifier to log connections
+	relayData.Network().Notify(&network.NotifyBundle{
+		ConnectedF: func(n network.Network, conn network.Conn) {
+			fmt.Printf("Peer connected: %s\n", conn.RemotePeer())
+		},
+		DisconnectedF: func(n network.Network, conn network.Conn) {
+			fmt.Printf("Peer disconnected: %s\n", conn.RemotePeer())
+		},
+	})
 
 	_, err = relay.New(relayData)
 	if err != nil {
